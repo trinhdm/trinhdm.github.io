613e4af15eba95739c5615a2cdad41f835bd3fad
trinhdm <doreenmtrinh@gmail.com>
2016-03-02 23:49:15 -0800

    Added files via upload

--- resume/js/particles-app.js
+++ resume/js/particles-app.js
@@ -23,7 +23,7 @@ particlesJS('particles-js',
         }
       },
       "color": {
-        "value": "#888888"
+        "value": "#aaaaaa"
       },
       "shape": {
         "type": "circle",
@@ -63,7 +63,7 @@ particlesJS('particles-js',
       "line_linked": {
         "enable": true,
         "distance": 150,
-        "color": "#888888",
+        "color": "#aaaaaa",
         "opacity": 0.4,
         "width": 1
       },
